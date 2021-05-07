861a09aaa378c190523f91d40e0e6df6742a5389
Leonardo Martin <81837338+leommartin@users.noreply.github.com>
2021-05-07 16:15:02 -0400

    commit program converte_segundos
    programa que converte segundos em horas, minutos e segundos

---
+++ Alg_1/aula_02/converte_segundos.pas
@@ -0,0 +1,12 @@
+program converteSeg;
+var 
+    seg,min,horas: longint;
+begin
+    write('Entre com uma quantidade de segundos: ');
+    read(seg);
+    horas:= seg div 3600;
+    seg:= seg mod 3600;
+    min:= seg div 60;
+    seg:= seg mod 60;
+    writeln(horas, 'h', min, 'm', seg, 'seg' )
+end.
