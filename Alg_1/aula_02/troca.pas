ddf5dd3f1e614a1636d6c262647b00ccd0005b25
Leonardo Martin <81837338+leommartin@users.noreply.github.com>
2021-05-07 15:27:56 -0400

    uso de readln

--- Alg_1/aula_02/troca.pas
+++ Alg_1/aula_02/troca.pas
@@ -2,7 +2,8 @@ program troca;
 var 
     a, b, aux: longint;
 begin
-    read(a,b);
+    read(a);
+    readln(b); // pega as primeiras infos da linha, só sai quando há um enter 
     aux:=a;
     a:= b;
     b:= aux;
