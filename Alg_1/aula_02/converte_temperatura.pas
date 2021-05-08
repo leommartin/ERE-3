f762ad0bfbdc2f56d24b8a64bb7c6db5865452ba
Leonardo Martin <81837338+leommartin@users.noreply.github.com>
2021-05-08 14:44:37 -0400

    Update converte_temperatura.pas

--- Alg_1/aula_02/converte_temperatura.pas
+++ Alg_1/aula_02/converte_temperatura.pas
@@ -1,9 +1,9 @@
 program converte_temperatura;
 var
-    f,c:integer;
+    f,c:real;
 begin
     write('Entre com uma temperatura em farenheit: ');
     read(f);
-    c:= (f-32) * 5 div 9;
-    writeln('Em celsius: ', c);
+    c:= 5/9 * (f-32); {agora as variaveis sao reais}
+    writeln('Em celsius: ', c:0:2); {saída em duas casas decimais}
 end.
