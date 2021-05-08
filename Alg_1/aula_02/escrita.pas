e7d08586e4372f05abbeb666604c34cca1b8499d
Leonardo Martin <81837338+leommartin@users.noreply.github.com>
2021-05-08 11:26:48 -0400

    Write e Writeln
    diferença de saída usando write e writeln

---
+++ Alg_1/aula_02/escrita.pas
@@ -0,0 +1,9 @@
+program escrita;
+begin
+	write('Segunda ');
+	write('Aula ');
+	writeln('de Alg 1'); {quebra de linha após a escrita}
+	writeln('Quarta, dia ', 15); {quebra de linha}
+	write('Otimo fim de semana a todos!'); {escreveu na linha após a quebra}
+	writeln;{outra quebra de linha}
+end.
